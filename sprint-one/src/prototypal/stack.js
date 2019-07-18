6c20323548b4fd344962e49bf95db50cd19d8bb1
Rey Mercado <reymercado.usa@gmail.com>
2019-07-18 08:32:00 -0700

    finished prototype!

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  // extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
 
+stackMethods.pop = function () {
+  var str = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1]];
+  return str;
+};
+
+stackMethods.size = function () {
+  return Object.keys(this.storage).length <= 0 ? 0 : Object.keys(this.storage).length;
+};
