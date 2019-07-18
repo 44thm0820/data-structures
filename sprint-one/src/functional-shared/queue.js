52a2b6de412875299edaaddaff63c0fbc12ae645
Rey Mercado <reymercado.usa@gmail.com>
2019-07-18 08:17:44 -0700

    functional-shared complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {
+    front: 0,
+    back: 0,
+    length: 0
+  };
+  extend(someInstance, queueMethods);
+  return someInstance;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.storage.back] = value;
+  this.storage.back += 1;
+  this.storage.length += 1;
+};
 
+queueMethods.dequeue = function () {
+  var str = this.storage[this.storage.front];
+  delete this.storage[this.storage.front];
+  this.storage.front += 1;
+  this.storage.length -= 1;
+  console.log(JSON.stringify(this.storage) + ' after dequeue');
+  return str;
+};
+
+queueMethods.size = function () {
+  return this.storage.length > 0 ? this.storage.length : 0;
+};
+
+// var queue = Queue();
+// queue.enqueue('a');
+// queue.enqueue('b');
+// queue.dequeue();
+// queue.enqueue('c');
+// console.log(queue.dequeue());
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,64 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  extend(someInstance, stackMethods);
+  return someInstance;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
 
+stackMethods.pop = function () {
+  var str = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage)[Object.keys(this.storage).length - 1]];
+  return str;
+};
+
+stackMethods.size = function () {
+  return Object.keys(this.storage).length <= 0 ? 0 : Object.keys(this.storage).length;
+};
+/*
+Functional instantiation with shared methods: same as step 1, but with shared methods
+
+Do:
+  Work within the src / functional - shared / folder
+Create an object that holds the methods that will be shared by all instances of the class
+Use the keyword this in your methods
+Use _.extend to copy the methods onto the instance
+Don 't:
+Use the keyword new or prototype chains
+Example: functional instantiation example
+
+var Stack = function () {
+  var someInstance = {}; //
+  // Use an object with numeric keys to store values
+  var storage = {};
+  // Implement the methods below
+  someInstance.push = function (value) {
+    // if (Object.keys(storage).length === 0) {
+    //   storage[0] = value;
+    // } else {
+    storage[Object.keys(storage).length] = value;
+    // }
+  };
+  someInstance.pop = function () {
+    var str = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage)[Object.keys(storage).length - 1]];
+    return str;
+  };
+
+  someInstance.size = function () {
+    return Object.keys(storage).length <= 0 ? 0 : Object.keys(storage).length;
+  };
+  return someInstance;
+};
+*/
