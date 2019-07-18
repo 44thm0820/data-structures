6ab91f5ec562f0fe7c80b5020b88aaa05a9109da
Rey Mercado <reymercado.usa@gmail.com>
2019-07-18 06:56:36 -0700

    functional complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,35 @@
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {front: 0, back: 0, length: 0};
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-  };
+    storage[storage.back] = value;
+    storage.back += 1;
+    storage.length += 1;
+    console.log(JSON.stringify(storage) + ' after enqueue');
 
+  };
   someInstance.dequeue = function() {
-  };
+    var str = storage[storage.front];
+    delete storage[storage.front];
+    storage.front += 1;
+    storage.length -= 1;
+    console.log(JSON.stringify(storage) + ' after dequeue');
+    return str;
 
-  someInstance.size = function() {
   };
-
+  someInstance.size = function () {
+    return storage.length > 0 ? storage.length : 0;
+  };
   return someInstance;
 };
+var queue = Queue();
+queue.enqueue('a');
+queue.enqueue('b');
+queue.dequeue();
+queue.enqueue('c');
+console.log(queue.dequeue());
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,23 @@
 var Stack = function() {
-  var someInstance = {};
-
+  var someInstance = {}; //
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
   someInstance.push = function(value) {
+    // if (Object.keys(storage).length === 0) {
+    //   storage[0] = value;
+    // } else {
+    storage[Object.keys(storage).length] = value;
+    // }
   };
-
   someInstance.pop = function() {
+    var str = storage[Object.keys(storage).length - 1];
+    delete storage[Object.keys(storage)[Object.keys(storage).length - 1]];
+    return str;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length <= 0 ? 0 : Object.keys(storage).length;
   };
-
   return someInstance;
 };
