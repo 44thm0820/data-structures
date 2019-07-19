578adf535683e0257bbf976408a41327579b1e88
44thm0820 <reymercado.usa@gmail.com>
2019-07-19 11:04:25 -0700

    linkedlist complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,22 +5,39 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     let newNode = Node(value);
-    if (list.head === null) {
+
+// 1. head and tail point to null (no nodes yet aka empty list)
+    if (list.head === null && list.tail ===  null) {
       list.head = newNode;
-    } else {
-      for (key in list) {
-        if (list[key].next === null) {
-          
-        }
-      }
+      list.tail = newNode;
+// 2. there is one node only in list (head and tail point to the same node)
+    } else  {
+      list.tail.next = newNode;
+      list.tail = newNode;
     }
-    
+
   };
 
   list.removeHead = function() {
+    let temp = list.head.value;
+    if (list.head !== null) { 
+      if (list.head === list.tail) {
+        list.head = null;
+        list.head = null;  
+      } else if (list.head !== list.tail) {
+        list.head = list.head.next;
+      }
+      return temp;
+    }
   };
 
   list.contains = function(target) {
+    for (let nd in list) {
+      if (list[nd].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -34,6 +51,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
