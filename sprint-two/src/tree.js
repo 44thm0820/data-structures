fa7617c90d0b2f4bbb57c9c4a3a288560118ece9
44thm0820 <reymercado.usa@gmail.com>
2019-07-19 20:36:17 -0700

    removed redundant boolean, improved name of variable

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -18,17 +18,16 @@ treeMethods.contains = function(target) {
   //base case: if node has no children, then return whether target is the same as the value
   //recursion: if node has any children, then 1) return if value is equal to target and 2) (recurse) check whether any of the children contain target
   console.log(this.children);
-  var bool = false;
+  var found = false;
   if (this.children.length === 0){
-    bool = bool || target === this.value;
+    found = found || target === this.value;
   } else {
-    bool = bool || this.value === target;
+    found = found || this.value === target;
     for (var child of this.children) {
-      bool = child.contains(target) || bool;
+      found = child.contains(target) || found;
     }    
-    return bool;
   }
-  return bool;
+  return found;
 };
 
 var oak = Tree("leaves");
